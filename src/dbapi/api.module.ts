4d3ac544cc0c873e7dc3581f68c1cb044030467e
Andrey <btr180@gmail.com>
2022-04-06 14:05:45 +0400

    add rbac

--- src/dbapi/api.module.ts
+++ src/dbapi/api.module.ts
@@ -4,9 +4,26 @@ import { PostsController } from './controllers/posts.controller';
 import { DatabaseModule } from './database/database.module';
 import { CommentsModule } from './modules/comments/comments.module';
 import { CommentsController } from './controllers/comments.controller';
+import { AuthModule } from './auth/auth.module';
+import { RolesModule } from './roles/roles.module';
+import { AuthController } from './auth/auth.controller';
+import { RolesController } from './roles/roles.controller';
+import { SessionsModule } from './modules/sessions/sessions.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './database/entities/user.entity';
+import { UsersRoles } from './database/entities/users-roles.entity';
+import { Role } from './database/entities/role.entity';
 
 @Module({
-  imports: [DatabaseModule, PostsModule, CommentsModule],
-  controllers: [PostsController, CommentsController],
+  imports: [
+    TypeOrmModule.forFeature([User, UsersRoles, Role]),
+    DatabaseModule,
+    PostsModule,
+    CommentsModule,
+    AuthModule,
+    RolesModule,
+    SessionsModule
+  ],
+  controllers: [PostsController, CommentsController, AuthController, RolesController],
 })
 export class ApiModule {}
--- src/dbapi/controllers/posts.controller.ts
+++ src/dbapi/controllers/posts.controller.ts
@@ -6,7 +6,11 @@ import {
   Post,
   Put,
   Query,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '../auth/auth-guard';
+import { Roles } from '../auth/roles-decorator';
+import { RolesGuard } from '../auth/roles-guard';
 import { Posts } from '../database/entities/post.entity';
 import { PostsDTO } from '../dto/post.dto';
 import { PostsService } from '../modules/posts/posts.service';
@@ -24,12 +28,17 @@ export class PostsController {
   async getPost(@Query() query: { id: number }): Promise<Posts | undefined> {
     return this.postsService.getPost(query.id);
   }
-
+  
+  @Roles("User")
+  @UseGuards(RolesGuard)
   @Post('create')
   async createPost(@Body() data: PostsDTO): Promise<Posts> {
     return this.postsService.createPost(data);
   }
 
+
+  @Roles("Admin")
+  @UseGuards(RolesGuard)
   @Delete('delete')
   async deletePost(@Body() body: { id: number }): Promise<Posts> {
     return this.postsService.deletePost(body.id);
